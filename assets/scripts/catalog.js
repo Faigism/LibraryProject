5dc95dfb642ba3ef92b3a0143f8849d9adc8e423
Pakizamutallim <62bq5tn@code.edu.az>
2024-05-08 21:13:41 +0400

    created carousel and added setinteral function

--- assets/scripts/catalog.js
+++ assets/scripts/catalog.js
@@ -1,26 +1,9 @@
-// let items = document.querySelectorAll('.carousel .carousel-inner')
-
-// items.forEach((el) => {
-//     // number of slides per carousel-item
-//     const minPerSlide = 2
-//     let next = el.nextElementSibling
-//     for (var i=1; i<minPerSlide; i++) {
-//         if (!next) {
-//             // wrap carousel by using first child
-//             next = items[0]
-//         }
-//         let cloneChild = next.cloneNode(true)
-//         el.appendChild(cloneChild.children[0])
-//         next = next.nextElementSibling
-//     }
-//     // console.log({next, minPerSlide, items});
-// })
-
 
 const carousel = document.querySelector('.carousel');
 const arrowBtns = document.querySelectorAll(".slider-wrapper i");
 const firstCardWidth = carousel.querySelector('.items').offsetWidth;
-
+const rightBtn = document.getElementById('right');
+const timePeriod = 1000;
 
 
 
@@ -30,9 +13,11 @@ arrowBtns.forEach(btn => {
     })
 })
 
+
+
 let isDragging = false, startX, startScrollLeft;
 
-const dragStart = () => {
+const dragStart = (e) => {
     isDragging = true;
     carousel.classList.add("dragging");
     startX = e.pageX;
@@ -41,7 +26,7 @@ const dragStart = () => {
 
 const dragging = (e) => {
     if (!isDragging) return;
-    carousel.scrollLeft = e.pageX;
+    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
 }
 
 const dragStop = () => {
@@ -49,6 +34,20 @@ const dragStop = () => {
     carousel.classList.remove("dragging");
 }
 
+const set = setInterval(() => {
+    rightBtn.click();
+    // console.log(carousel.scrollLeft);
+    if (carousel.scrollLeft >= (carousel.scrollWidth - 966)) {
+        console.log("test");
+        // clearInterval(set);
+        carousel.scrollLeft = 0
+    }
+}, timePeriod);
+
+
+
 carousel.addEventListener('mousedown', dragStart);
 carousel.addEventListener('mousemove', dragging);
 carousel.addEventListener('mouseup', dragStop);
+
+
