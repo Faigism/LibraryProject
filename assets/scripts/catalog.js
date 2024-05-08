810d50de192de9f2d49198fd7fe3485dcc4c5420
PakizaMutallimova <pakizemutellimova97@gmail.com>
2024-05-08 17:42:34 +0400

    created carousel

--- assets/scripts/catalog.js
+++ assets/scripts/catalog.js
@@ -18,8 +18,37 @@
 
 
 const carousel = document.querySelector('.carousel');
-const dragging = (e) => {
-    console.log(e.pageX);
+const arrowBtns = document.querySelectorAll(".slider-wrapper i");
+const firstCardWidth = carousel.querySelector('.items').offsetWidth;
+
+
+
+
+arrowBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
+    })
+})
+
+let isDragging = false, startX, startScrollLeft;
+
+const dragStart = () => {
+    isDragging = true;
+    carousel.classList.add("dragging");
+    startX = e.pageX;
+    startScrollLeft = carousel.scrollLeft
 }
 
-carousel.addEventListener('mousemove', dragging)
+const dragging = (e) => {
+    if (!isDragging) return;
+    carousel.scrollLeft = e.pageX;
+}
+
+const dragStop = () => {
+    isDragging = false;
+    carousel.classList.remove("dragging");
+}
+
+carousel.addEventListener('mousedown', dragStart);
+carousel.addEventListener('mousemove', dragging);
+carousel.addEventListener('mouseup', dragStop);
