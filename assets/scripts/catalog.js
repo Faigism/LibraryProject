9ee73b86c2fdee3e940c69c75e0b6d3d7dc7240f
PakizaMutallimova <pakizemutellimova97@gmail.com>
2024-05-09 23:16:56 +0400

    added all sections but didnt finish yet

--- assets/scripts/catalog.js
+++ assets/scripts/catalog.js
@@ -4,16 +4,13 @@ const firstCardWidth = carousel.querySelector('.items').offsetWidth;
 const rightBtn = document.getElementById('right');
 const timePeriod = 1000;
 
-
-
+let isDragging = false, startX, startScrollLeft;
 
 arrowBtns.forEach(btn => {
     btn.addEventListener("click", () => {
         carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
     })
 })
-
-let isDragging = false, startX, startScrollLeft;
 
 const dragStart = (e) => {
     isDragging = true;
@@ -36,9 +33,9 @@ const dragStop = () => {
 const set = setInterval(() => {
     rightBtn.click();
     // console.log(carousel.scrollLeft);
-    if (carousel.scrollLeft >= (carousel.scrollWidth - 966)) {
-        carousel.scrollLeft = 0;
-    }
+    // if (carousel.scrollLeft >= (carousel.scrollWidth - 966)) {
+    //     carousel.scrollLeft = 0;
+    // }
 }, timePeriod);
 
 carousel.addEventListener('mousedown', dragStart);
