16b95041de21107f9c01f73d250b25cace7f3b52
BayramBehbudov <bayram.behbudov@gmail.com>
2024-05-10 17:45:27 +0400

    some changes

--- assets/scripts/adminpanel.js
+++ assets/scripts/adminpanel.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js'
-import { getAnalytics } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-analytics.js'
+
 import {
   getDatabase,
   ref,
